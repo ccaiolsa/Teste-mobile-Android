12a3a80e55990e63c43d76c01c39c08e1f5a7823
ccaiolsa <ccaiolsa@gmail.com>
2026-07-27 20:56:27 -0400

    workflow

--- config/wdio.conf.js
+++ config/wdio.conf.js
@@ -1,8 +1,15 @@
 import 'dotenv/config'
 import {localConf} from './local.conf.js'
 import {sauceConf} from './sauce.conf.js'
+console.log('ENVIRONMENT =', process.env.ENVIRONMENT)
+    const cfg = getConf()
+
+console.log(cfg)
+
+export const config = cfg
 
 function getConf(){
+    
     switch(process.env.ENVIRONMENT){
         case 'local':
             return localConf
